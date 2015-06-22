439a62906a5b839caa4d4141deeff59c34fbe251
caellian <tin.svagelj.email@gmail.com>
2015-06-22 17:19:04 +0200

    Updated copyright for this project. It is not officially closed and discontinued. A new project will hopefully emerge from this code. Goodbye Atlas.

--- src/com/caellyan/core/ArgumentHelper.java
+++ src/com/caellyan/core/ArgumentHelper.java
@@ -1,19 +1,15 @@
 /*
- * CaellianCore, universal library
- * Copyright (C) 2015 Caellian
+ * Atlas Conscientia, artificial consciousness
+ * Copyright (c) 2015 Skythees
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This project has been abandoned and thus you can redistribute it and/or modify
+ * it under no terms.
  *
- * This program is distributed in the hope that it will be useful,
+ * This project is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * It might or is not in functioning state. Skythees isn't and will not be held
+ * responsible for any consequences it might have on any software/hardware.
  */
 
 package com.caellyan.core;
